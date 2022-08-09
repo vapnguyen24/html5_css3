ef8a401a4ff57904b42a466a936e380a90c2a2b0
vapnguyen24 <vinhnt.work@gmail.com>
2022-08-09 07:51:14 +0700

    modify background zise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-const $ = document.querySelector.bind(document); 
+const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 // Scroll Top
 var scrollTop = () => {
-    var header = $(".header");
-    header.scrollIntoView({ behavior: "smooth", block: "start" });
+  var header = $(".header");
+  header.scrollIntoView({ behavior: "smooth", block: "start" });
 };
 
 var scrollBtn = $(".scroll-top");
@@ -11,14 +11,14 @@ scrollBtn.addEventListener("click", scrollTop);
 window.addEventListener("scroll", scrollBtnDisplay);
 
 function scrollBtnDisplay() {
-    if (
-        document.body.scrollTop > 200 ||
-        document.documentElement.scrollTop > 200
-    ) {
-        scrollBtn.style.display = "block";
-    } else {
-        scrollBtn.style.display = "none";
-    }
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
 }
 
 // Active link header
@@ -28,9 +28,9 @@ var listLength = listItem.length;
 
 var i = 0;
 for (i; i < listLength; i++) {
-    if (listItem[i].href === listLink) {
-        listItem[i].className = "active";
-    }
+  if (listItem[i].href === listLink) {
+    listItem[i].className = "active";
+  }
 }
 
 // var links = location.href;
@@ -49,100 +49,97 @@ var headerSticky = $(".header__nav");
 window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        headerSticky.classList.add("scroll");
-    } else {
-        headerSticky.classList.remove("scroll");
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    headerSticky.classList.add("scroll");
+  } else {
+    headerSticky.classList.remove("scroll");
+  }
 }
 
 // Search Button
 var searchIcon = $("#search");
 var input = $(".header__nav__container--search input");
 
-searchIcon.onclick = function() {
-    input.classList.toggle("active");
+searchIcon.onclick = function () {
+  input.classList.toggle("active");
 };
 
 // Active link
 var links = $$(".header__nav__container--list li");
-var add = function() {
-    $(".header__nav__container--list li.active").classList.remove("active");
-    this.classList.add("active");
+var add = function () {
+  $(".header__nav__container--list li.active").classList.remove("active");
+  this.classList.add("active");
 };
 links.forEach((link) => {
-    link.addEventListener("click", add);
+  link.addEventListener("click", add);
 });
-
 
 // Active Search
 var tabs = $$(".search__container__tab");
 
 tabs.forEach((tab) => {
-    tab.onclick = function() {
-        $(".search__container__tab.active").classList.remove("active");
-        this.classList.add("active");
-    };
+  tab.onclick = function () {
+    $(".search__container__tab.active").classList.remove("active");
+    this.classList.add("active");
+  };
 });
 
 // Header menu modal
-
 
 var box = $(".header__menu__overlay");
 var boxBody = $(".header__menu__body--content");
 var open = $("#menu_open");
 var close = $("#menu_close");
 
-open.onclick = function() {
-    box.style.display = "block";
-    boxBody.classList.add("open");
+open.onclick = function () {
+  box.style.display = "block";
+  boxBody.classList.add("open");
 };
 
-close.onclick = function() {
-    box.style.display = "none";
-    boxBody.classList.remove("open");
+close.onclick = function () {
+  box.style.display = "none";
+  boxBody.classList.remove("open");
 };
 
 // signin
-var email = document.getElementById('mail');
-var password = document.getElementById('password');
-var rePassword = document.getElementById('repassword');
-var er1 = document.getElementById('error1');
-var er2 = document.getElementById('error2');
-var er3 = document.getElementById('error3');
+var email = document.getElementById("mail");
+var password = document.getElementById("password");
+var rePassword = document.getElementById("repassword");
+var er1 = document.getElementById("error1");
+var er2 = document.getElementById("error2");
+var er3 = document.getElementById("error3");
 function valid() {
-    var flag = 0;
-    if (email.value.length == 0)
-        er1.innerHTML = 'Vui lòng nhập vào ô đang trống';
-    else if (!ValidateEmail(email.value))
-        er1.innerHTML = 'Vui lòng nhập đúng định dạng';
-    else {
-        er1.innerHTML = '';
-        flag++;
-    }
+  var flag = 0;
+  if (email.value.length == 0) er1.innerHTML = "Vui lòng nhập vào ô đang trống";
+  else if (!ValidateEmail(email.value))
+    er1.innerHTML = "Vui lòng nhập đúng định dạng";
+  else {
+    er1.innerHTML = "";
+    flag++;
+  }
 
-    if (password.value.length == 0) {
-        er2.innerHTML = 'Vui lòng nhập vào ô đang trống';
-    } else if (password.value.length < 6)
-        er2.innerHTML = 'Mật khẩu phải có ít nhất 6 ký tự';
-    else {
-        er2.innerHTML = '';
-        flag++
-    }
+  if (password.value.length == 0) {
+    er2.innerHTML = "Vui lòng nhập vào ô đang trống";
+  } else if (password.value.length < 6)
+    er2.innerHTML = "Mật khẩu phải có ít nhất 6 ký tự";
+  else {
+    er2.innerHTML = "";
+    flag++;
+  }
 
-    if (rePassword.value.length == 0) {
-        er3.innerHTML = 'Vui lòng nhập vào ô đang trống';
-    } else if (rePassword.value !== password.value) {
-        er3.innerHTML = 'Mật khẩu không trùng khớp';
-    } else {
-        er3.innerHTML = '';
-        flag++;
-    }
-
-
+  if (rePassword.value.length == 0) {
+    er3.innerHTML = "Vui lòng nhập vào ô đang trống";
+  } else if (
+    rePassword.value !== password.value ||
+    rePassword.value.length < 6
+  ) {
+    er3.innerHTML = "Mật khẩu không trùng khớp";
+  } else {
+    er3.innerHTML = "";
+    flag++;
+  }
 }
 
-function ValidateEmail(mail) 
-{
-    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+function ValidateEmail(mail) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail);
 }
